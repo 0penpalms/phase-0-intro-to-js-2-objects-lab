8442b4f8e7fd99c4cbd0ac3c922e589b6239bad3
0penpalms <jamescartermvnyc@gmail.com>
2024-07-26 14:49:42 -0400

    All tests passing.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {};
+
+function deleteFromEmployeeByKey(employee, key) {
+    let copy = {
+        ...employee
+    };
+
+    delete copy[key];
+
+    return copy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    
+    return employee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+     employee[key] = value;
+     return employee;
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    }
+};
